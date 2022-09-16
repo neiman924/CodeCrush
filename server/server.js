df04cae44a285fd17500b56da4e7ff2db18560ea
Hossein Neiman <neiman924@gmail.com>
2022-09-15 22:29:51 -0700

    geting ready to heroku

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
-// const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-
 const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
+const express = require('express');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,11 +21,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
